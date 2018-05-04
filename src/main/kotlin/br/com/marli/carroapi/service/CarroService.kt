717378368253a292b9cd572fce03f289e681a83d
hagape <hagape@gmail.com>
2018-05-03 21:13:19 -0300

    Update CarroService.kt

--- src/main/kotlin/br/com/marli/carroapi/service/CarroService.kt
+++ src/main/kotlin/br/com/marli/carroapi/service/CarroService.kt
@@ -6,30 +6,28 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class CarroService{
+class CarroService {
 
     @Autowired
-    lateinit var carroRepository: CarroRepository
+    lateinit var carroRepository : CarroRepository
 
-    fun buscarTodosCarros(): List<Carro>{
+    fun buscarTodosCarros() : List<Carro>{
         return carroRepository.findAll()
     }
 
-    fun buscarCarroPorMarca(marca:String): List<Carro>{
-        return carroRepository.findByMarcaContaining(marca)
-
-    }
-
-    fun buscarCarroPorPlaca(placa:String):List<Carro>{
-        return carroRepository.findByPlaca(placa)
-    }
-
-    fun buscarcarroPorAno(ano: Int): List<Carro>{
+    fun burcarCarroPorAno(ano: Int) : List<Carro>{
         return carroRepository.findByAno(ano)
+    }
+
+    fun buscarCarroPorMarca(marca: String) : List<Carro>{
+        return carroRepository.findByMarcaContaining(marca)
+    }
+
+    fun buscarCarroPorPlaca(placa: String) : Carro{
+        return carroRepository.findByPlacaContaining(placa)
     }
 
     fun salvar(carro: Carro){
         carroRepository.save(carro)
     }
-
 }
